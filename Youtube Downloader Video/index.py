2f287955bb57ba01bf4acde5ad3a681e0498cfba
Apri Andriyanto N <47985244+apriandri@users.noreply.github.com>
2022-07-04 17:14:22 +0700

    add new files

---
+++ Youtube Downloader Video/index.py
@@ -0,0 +1,28 @@
+'''
+YouTube Video Downloader
+Author: Ayushi Rawat
+'''
+
+#import the package
+from pytube import YouTube
+
+url = 'Your URL goes here'
+my_video = YouTube(url)
+
+print("+++++++++++++Title+++++++++++++")
+#get Video Title
+print(my_video.title)
+
+print("********************Tumbnail+++++++++++++")
+# Thumbnail Image
+print(my_video.thumbnail_url)
+
+print("********************Downloaded video+++++++++++++")
+#get all the stream resolution for the 
+for stream in my_video.streams:
+    print(stream)
+
+#set stream resolution
+my_video = my_video.streams.get_highest_resolution()
+
+my_video.download()
